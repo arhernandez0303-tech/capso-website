8af9c7bbe945d49d421b7df0cf89436402f5160a
TonyTheTitan <arhernandez0303@gmail.com>
2026-02-27 13:11:12 -0600

    Update LinkedIn URL for Director of Marketing

--- src/pages/StateCommission.tsx
+++ src/pages/StateCommission.tsx
@@ -15,7 +15,7 @@ const leadershipRoles = [
   { title: "Associate Director", name: "Cohen Moncada", image: cohenImg, linkedin: "https://www.linkedin.com/in/cohen-moncada-753540366/" },
   { title: "Director of Organizational Development", name: "Enrico Metzger", image: enricoImg, linkedin: "https://www.linkedin.com/in/enricoo-metzgerr/" },
   { title: "Director of Programs", name: "Lia Tovar", image: liaImg, linkedin: "https://linkedin.com/in/" },
-  { title: "Director of Marketing", name: "Faris Ebrahim", image: farisImg, linkedin: "https://linkedin.com/in/" },
+  { title: "Director of Marketing", name: "Faris Ebrahim", image: farisImg, linkedin: "https://www.linkedin.com/in/faris-ebrahim-165582316/" },
   { title: "Director of Finance", name: "Josh Petite", image: joshImg, linkedin: "https://linkedin.com/in/" },
 ];
 
